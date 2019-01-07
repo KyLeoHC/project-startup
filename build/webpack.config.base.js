7579bb97fe09c1bc3645d628c07ec27676ff44dc
KyLeoHC <kyleo.hc@foxmail.com>
2019-01-07 15:18:45 +0800

    update

--- build/webpack.config.base.js
+++ build/webpack.config.base.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
-const devMode = process.env.BUILD_ENV === 'dev';
+const devMode = process.env.BUILD_ENV === 'development';
 
 const config = {
     entry: {},
