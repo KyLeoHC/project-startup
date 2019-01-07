270caa45f14db4a453106b6b4b87d609890f2e9b
KyLeoHC <kyleo.hc@foxmail.com>
2019-01-07 16:58:06 +0800

    update

--- build/config.js
+++ build/config.js
@@ -2,7 +2,7 @@ module.exports = {
     cleanFiles: ['./dist.zip', './dist', './dist-tpc', './dist-tpc.zip'],
     outputDirectory: 'dist',
     publicPathMap: {
-        dev: '/dev',
+        development: '/dev',
         test: '/dist/',
         prev: '/dist/',
         production: '/project-startup/dist/'
